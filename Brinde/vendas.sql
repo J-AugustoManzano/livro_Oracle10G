988902f88723e51f7baad122760344c67a5e415e
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2021-05-05 16:08:35 -0300

    Add files via upload

---
+++ Brinde/vendas.sql
@@ -0,0 +1,349 @@
+INSERT INTO venda VALUES (
+  '100000',
+  5060.88,
+  '15/05/1999',
+  '300'
+);
+
+INSERT INTO venda VALUES (
+  '100334',
+  2002.50,
+  '20/12/2005',
+  '990'
+);
+
+INSERT INTO venda VALUES (
+  '100335',
+  2002.50,
+  '20/01/2006',
+  '990'
+);
+
+INSERT INTO venda VALUES (
+  '100336',
+  2002.50,
+  '20/02/2006',
+  '990'
+);
+
+INSERT INTO venda VALUES (
+  '101010',
+  5060.88,
+  '15/03/2000',
+  '300'
+);
+
+INSERT INTO venda VALUES (
+  '101110',
+  3460.66,
+  '15/04/2000',
+  '300'
+);
+
+INSERT INTO venda VALUES (
+  '101210',
+  9060.55,
+  '15/10/2001',
+  '300'
+);
+
+INSERT INTO venda VALUES (
+  '111999',
+  1250.00,
+  '15/04/2005',
+  '150'
+);
+
+INSERT INTO venda VALUES (
+  '112099',
+  1000.00,
+  '22/06/2005',
+  '150'
+);
+
+INSERT INTO venda VALUES (
+  '112199',
+  1350.00,
+  '30/08/2005',
+  '150'
+);
+
+INSERT INTO venda VALUES (
+  '112299',
+  1000.00,
+  '15/10/2005',
+  '150'
+);
+
+INSERT INTO venda VALUES (
+  '121212',
+  1300.00,
+  '15/01/2002',
+  '170'
+);
+
+INSERT INTO venda VALUES (
+  '121213',
+  1000.00,
+  '15/02/2002',
+  '170'
+);
+
+INSERT INTO venda VALUES (
+  '121214',
+  1400.00,
+  '15/03/2002',
+  '170'
+);
+
+INSERT INTO venda VALUES (
+  '121515',
+  1000.00,
+  '15/10/2003',
+  '230'
+);
+
+INSERT INTO venda VALUES (
+  '121616',
+  1600.00,
+  '15/11/2003',
+  '230'
+);
+
+INSERT INTO venda VALUES (
+  '155099',
+  1800.00,
+  '13/11/2004',
+  '250'
+);
+
+INSERT INTO venda VALUES (
+  '155199',
+  1000.00,
+  '15/10/2003',
+  '250'
+);
+
+INSERT INTO venda VALUES (
+  '155299',
+  1900.00,
+  '15/11/2003',
+  '250'
+);
+
+INSERT INTO venda VALUES (
+  '155999',
+  1000.00,
+  '14/10/2004',
+  '250'
+);
+
+INSERT INTO venda VALUES (
+  '210001',
+  6000.00,
+  '12/04/1999',
+  '100'
+);
+
+INSERT INTO venda VALUES (
+  '230002',
+  1300.00,
+  '10/09/2005',
+  '550'
+);
+
+INSERT INTO venda VALUES (
+  '231015',
+  1800.00,
+  '10/10/2005',
+  '550'
+);
+
+INSERT INTO venda VALUES (
+  '250013',
+  2002.00,
+  '10/03/2002',
+  '820'
+);
+
+INSERT INTO venda VALUES (
+  '400400',
+  6000.00,
+  '15/03/2003',
+  '340'
+);
+
+INSERT INTO venda VALUES (
+  '451300',
+  1750.00,
+  '22/05/1999',
+  '800'
+);
+
+INSERT INTO venda VALUES (
+  '460460',
+  1902.50,
+  '20/02/2004',
+  '100'
+);
+
+INSERT INTO venda VALUES (
+  '460560',
+  1902.50,
+  '22/03/2004',
+  '100'
+);
+
+INSERT INTO venda VALUES (
+  '460660',
+  1902.50,
+  '24/04/2004',
+  '100'
+);
+
+INSERT INTO venda VALUES (
+  '500005',
+  5060.88,
+  '15/05/1999',
+  '990'
+);
+
+INSERT INTO venda VALUES (
+  '600099',
+  1300.00,
+  '13/10/2001',
+  '860'
+);
+
+INSERT INTO venda VALUES (
+  '600199',
+  1400.00,
+  '15/11/2002',
+  '860'
+);
+
+INSERT INTO venda VALUES (
+  '600299',
+  1500.00,
+  '05/12/2002',
+  '860'
+);
+
+INSERT INTO venda VALUES (
+  '600999',
+  1200.00,
+  '14/09/2001',
+  '860'
+);
+
+INSERT INTO venda VALUES (
+  '653099',
+  1350.88,
+  '13/09/2003',
+  '750'
+);
+
+INSERT INTO venda VALUES (
+  '653199',
+  1432.77,
+  '15/08/2004',
+  '750'
+);
+
+INSERT INTO venda VALUES (
+  '653999',
+  1255.99,
+  '14/09/2003',
+  '750'
+);
+
+INSERT INTO venda VALUES (
+  '750299',
+  1560.00,
+  '03/12/2004',
+  '800'
+);
+
+INSERT INTO venda VALUES (
+  '751299',
+  1570.00,
+  '05/12/2005',
+  '800'
+);
+
+INSERT INTO venda VALUES (
+  '750300',
+  1565.00,
+  '05/11/2004',
+  '800'
+);
+
+INSERT INTO venda VALUES (
+  '752252',
+  1675.00,
+  '15/12/2006',
+  '820'
+);
+
+INSERT INTO venda VALUES (
+  '752253',
+  1675.00,
+  '22/11/2006',
+  '820'
+);
+
+INSERT INTO venda VALUES (
+  '950888',
+  2002.50,
+  '10/07/2006',
+  '500'
+);
+
+INSERT INTO venda VALUES (
+  '950889',
+  2002.50,
+  '10/08/2006',
+  '500'
+);
+
+INSERT INTO venda VALUES (
+  '950890',
+  2002.50,
+  '10/09/2006',
+  '500'
+);
+
+INSERT INTO venda VALUES (
+  '950891',
+  2002.50,
+  '10/10/2006',
+  '500'
+);
+
+INSERT INTO venda VALUES (
+  '202055',
+  2390.00,
+  '11/11/2004',
+  '550'
+);
+
+INSERT INTO venda VALUES (
+  '302056',
+  2490.00,
+  '20/10/2006',
+  '550'
+);
+
+INSERT INTO venda VALUES (
+  '111818',
+  3000.00,
+  '11/09/2006',
+  '170'
+);
+
+INSERT INTO venda VALUES (
+  '222550',
+  9518.55,
+  '21/11/2005',
+  '750'
+);
